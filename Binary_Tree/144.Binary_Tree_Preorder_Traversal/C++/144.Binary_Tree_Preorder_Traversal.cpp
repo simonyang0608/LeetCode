0d6e665d7d4814fa42951fd602945eadd892d6a5
simon <31026907+simonyang0608@users.noreply.github.com>
2024-08-24 13:46:48 +0800

    [Add] Add binary-tree title no.3 with c++ language

---
+++ Binary_Tree/144.Binary_Tree_Preorder_Traversal/C++/144.Binary_Tree_Preorder_Traversal.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfsTraversal(TreeNode * root, vector<int> & res_vect)
+    {
+        //======================================//
+        // Input type:                          //
+        //   - TreeNode * (root tree-node)      //
+        //   - vector<int> ref. (result vector) //
+        // Return type:                         //
+        //   - void (no return)                 //
+        //======================================//
+        if (! root) { return; } //Issue, Boundary-case handling
+
+        /*Whole process, flow*/
+        res_vect.push_back((root->val)); //Update, Record
+
+        dfsTraversal((root->left), res_vect); //Resursion function call (i.e. root left-half node)
+        dfsTraversal((root->right), res_vect); //Resursion function call (i.e. root right-half node)
+    }
+
+
+    vector<int> preorderTraversal(TreeNode * root) 
+    {
+        //=================================//
+        // Input type:                     //
+        //   - TreeNode * (root tree-node) //
+        // Return type:                    //
+        //   - vector<int> (result vector) //
+        //=================================//
+
+        /*Initialize*/
+        ///// Result vector /////
+        vector<int> res_vect;
+
+        /*Recursion-based DFS loop traversal*/
+        dfsTraversal(root, res_vect); //Recursion function call (i.e. whole node)
+
+        return res_vect;
+    }
+};
