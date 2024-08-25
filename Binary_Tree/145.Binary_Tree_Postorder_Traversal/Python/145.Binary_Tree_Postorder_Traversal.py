0344c709207b1483e37128611357ac292ceed121
simon <31026907+simonyang0608@users.noreply.github.com>
2024-08-25 11:48:31 +0800

    [Add] Add binary-tree title no.6 with python language

---
+++ Binary_Tree/145.Binary_Tree_Postorder_Traversal/Python/145.Binary_Tree_Postorder_Traversal.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        #======================================#
+        # Recursion-based DFS traversal method #
+        #======================================#
+
+        ############
+        #Initialize
+        ##### Result array #####
+        res_arry = []
+
+        ####################################
+        #Recursion-based DFS loop traversal
+        self.dfsTraversal(root, res_arry) #Recursion function call (i.e. whole node)
+
+        return res_arry
+
+
+    def dfsTraversal(self, root, res_arry):
+        """
+        :type root: TreeNode
+        :type res_arry: List[int]
+        :rtype: None, void
+        """
+        #========================================#
+        # Recursion-based DFS traversal function #
+        #========================================#
+        if (not root): #Issue/Boundary-case handling
+            return
+
+        ####################
+        #Whole process/flow
+        self.dfsTraversal((root.left), res_arry) #Recursion function call (i.e. root left-half node)
+        self.dfsTraversal((root.right), res_arry) #Recursion function call (i.e. root right-half node)
+
+        res_arry.append((root.val)) #Update/Record
