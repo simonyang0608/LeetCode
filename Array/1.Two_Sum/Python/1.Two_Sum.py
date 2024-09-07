c1a6770f27acf48524310a1feb223f7ae135b75f
simon <31026907+simonyang0608@users.noreply.github.com>
2024-09-07 10:57:54 +0800

    [Add] Add array title no.8 with python language

---
+++ Array/1.Two_Sum/Python/1.Two_Sum.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        #===============================#
+        # Greedy-based traversal method #
+        #===============================#
+
+        ############
+        #Initialize
+        ##### Length of nums #####
+        len_nums = len(nums)
+
+        ##### Record dictionary/hashmap #####
+        record_dict = {}
+
+
+        ##############################################################
+        #Greedy-based loop traversal with recorded dictionary/hashmap
+        for nums_idx in range(len_nums):
+
+            ##### Check if the current indexed-value existed or not #####
+            if ((target - nums[nums_idx]) in record_dict):
+                return [record_dict[(target - nums[nums_idx])], nums_idx]
+            else:
+                pass
+            
+            record_dict[nums[nums_idx]] = nums_idx #Keep updating/recording
+
+        return [(-1), (-1)]
