98b93b7528775ecf66be1e424619be93912be28c
simon <31026907+simonyang0608@users.noreply.github.com>
2024-11-09 13:28:43 +0800

    [Modify] Modify array title to no.20 with python language

--- Array/1658.Minimum_Operations_to_Reduce_X_to_Zero/Python/1658.Minimum_Operations_to_Reduce_X_to_Zero.py
+++ Array/1658.Minimum_Operations_to_Reduce_X_to_Zero/Python/1658.Minimum_Operations_to_Reduce_X_to_Zero.py
@@ -2,7 +2,7 @@ class Solution(object):
     def minOperations(self, nums, x):
         """
         :type nums: List[int]
-        :type x: int
+        :type x: int 
         :rtype: int
         """
         #=======================================#
