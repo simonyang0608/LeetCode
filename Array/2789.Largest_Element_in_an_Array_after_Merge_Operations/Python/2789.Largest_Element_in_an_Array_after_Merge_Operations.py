9ea7caf4b313fc11024da9736643c1cd5017dbef
simon <31026907+simonyang0608@users.noreply.github.com>
2024-11-09 13:27:17 +0800

    [Modify] Modify array title to no.21 with python language

--- Array/2789.Largest_Element_in_an_Array_after_Merge_Operations/Python/2789.Largest_Element_in_an_Array_after_Merge_Operations.py
+++ Array/2789.Largest_Element_in_an_Array_after_Merge_Operations/Python/2789.Largest_Element_in_an_Array_after_Merge_Operations.py
@@ -2,7 +2,7 @@ class Solution(object):
     def maxArrayValue(self, nums):
         """
         :type nums: List[int]
-        :rtype: int
+        :rtype: int 
         """
         #===============================#
         # Greedy-based traversal method #
