02901e42345284211817018d65c23595d901f2c1
simon <31026907+simonyang0608@users.noreply.github.com>
2024-11-09 13:21:29 +0800

    [Add] Add array title no.15 with python language

---
+++ Array/2789.Largest_Element_in_an_Array_after_Merge_Operations/Python/2789.Largest_Element_in_an_Array_after_Merge_Operations.py
@@ -0,0 +1,46 @@
+class Solution(object):
+    def maxArrayValue(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #===============================#
+        # Greedy-based traversal method #
+        #===============================#
+
+        ############
+        #Initialize
+        ##### Length of nums array #####
+        len_nums = len(nums)
+
+        ##### Record right pointer #####
+        record_right_ptr = (len_nums - 1)
+
+        ##### Record summary value #####
+        record_sum_val = 0
+
+        ##### Result maximun summary value #####
+        res_max_sum_val = 0
+
+
+        #############################
+        #Greedy-based loop traversal
+        while (record_right_ptr >= 0):
+            record_sum_val += nums[record_right_ptr] #Keep updating/accumulating
+
+            while (((record_right_ptr - 1) >= 0) and (record_sum_val >= nums[(record_right_ptr - 1)])):
+                record_sum_val += nums[(record_right_ptr - 1)] #Keep updating/accumulating
+
+                record_right_ptr -= 1 #Keep updating/reducing
+
+            res_max_sum_val = max(res_max_sum_val, record_sum_val) #Keep updating/overwriting
+
+            ##### Check if the current summary value needs to be reset or not #####
+            if (record_sum_val):
+                record_sum_val &= 0 #Reset
+            else:
+                pass
+
+            record_right_ptr -= 1 #Keep updating/reducing
+
+        return res_max_sum_val
