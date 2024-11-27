e1a7377984e738f7c74352f21e9367027c67d3c6
simon <31026907+simonyang0608@users.noreply.github.com>
2024-11-27 23:29:37 +0800

    [Modify] Modify depth-first-search title no.3 with c++ language

--- Depth_First_Search/1376.Time_Needed_to_Inform_All_Employees/C++/1376.Time_Needed_to_Inform_All_Employees.cpp
+++ Depth_First_Search/1376.Time_Needed_to_Inform_All_Employees/C++/1376.Time_Needed_to_Inform_All_Employees.cpp
@@ -2,32 +2,32 @@ class Solution {
 public:
     int numOfMinutes(int n, int headID, vector<int> & manager, vector<int> & informTime) 
     {
-        //=======================================//
-        // Input type:                           //
-        //   - int (n employees)                 //
-        //   - int (headID)                      //
-        //   - vector<int> ref. (manager vector) //
-        //   - vector<int> & (informTime vector) //
-        // Return type:                          //
-        //   - int (result maximun time)         //
-        //=======================================//
+        //==========================================//
+        // Input type:                              //
+        //   - int (n employees)                    //
+        //   - int (headID)                         //
+        //   - vector<int> ref. (manager vector)    //
+        //   - vector<int> ref. (informTime vector) //
+        // Return type:                             //
+        //   - int (result maximun time)            //
+        //==========================================//
 
         /*Initialize*/
         ///// Record dictionary, hashmap /////
         unordered_map<int, vector<pair<int, int>>> record_map;
 
-        /*Recursion-based DFS loop traversal with recorded dictionary, hashmap*/
+        /*Recursion-based DFS & BFS loop traversal with recorded dictionary, hashmap*/
         for (int n_idx = 0; (n_idx < n); (n_idx++)) //Whole
         {
             (record_map[manager[n_idx]]).emplace_back(make_pair(n_idx, informTime[n_idx])); //Keep updating, recording
 
         } //Whole
 
-        return dfsTraversal(record_map, (-1), 0); //Recursion function call
+        return dfsbfsTraversal(record_map, (-1), 0); //Recursion function call
     }
 
 
-    int dfsTraversal(unordered_map<int, vector<pair<int, int>>> & record_map, int record_manager, int record_time)
+    int dfsbfsTraversal(unordered_map<int, vector<pair<int, int>>> & record_map, int record_manager, int record_time)
     {
         //==================================================================================//
         // Input type:                                                                      //
@@ -46,7 +46,7 @@ public:
         /*Whole process, flow*/
         for (pair<int, int> record_pair: record_map[record_manager]) //Whole
         {
-            tmp_time = dfsTraversal(record_map, record_pair.first, (record_time + record_pair.second)); //Recursion function call
+            tmp_time = dfsbfsTraversal(record_map, record_pair.first, (record_time + record_pair.second)); //Recursion function call
 
             res_max_time = (tmp_time > res_max_time) ? tmp_time : res_max_time; //Keep updating, overwriting
 
