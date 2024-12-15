f69a8e295c24a19c737b376f78de7516a187372d
simon <31026907+simonyang0608@users.noreply.github.com>
2024-12-15 13:52:57 +0800

    [Add] Add string title no.23 with c++ language

---
+++ String/2337.Move_Pieces_to_Obtain_a_String/C++/2337.Move_Pieces_to_Obtain_a_String.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    bool canChange(string start, string target) 
+    {
+        //============================//
+        // Input type:                //
+        //   - string (start string)  //
+        //   - string (target string) //
+        // Return type:               //
+        //   - bool (result flag)     //
+        //============================//
+
+        /*Initialize*/
+        ///// Length of strings (i.e. start, target) /////
+        int len_string = start.size();
+
+        ///// Record stacks (i.e. left, right) /////
+        stack<char> record_left_stack, record_right_stack;
+
+
+        /*Stack-based loop traversal*/
+        for (int string_idx = 0; (string_idx < len_string); (string_idx++)) //Left
+        {
+            ///// Check if the current indexed-char matched conditions or not /////
+            if (target[string_idx] == 'L') { record_left_stack.push('L'); } //Keep updating, recording
+            else { ; }
+
+            ///// Check if the current stack is empty or not /////
+            if (! record_left_stack.empty())
+            {
+                ///// Check if the current indexed-char matched conditions or not /////
+                if ((start[string_idx] == 'R') || (target[string_idx] == 'R')) { return false; }
+                else if (start[string_idx] == 'L') { record_left_stack.pop(); } //Keep updating, popped
+                else { ; }
+            }
+
+            else
+            {
+                ///// Check if the current indexed-char matched conditions or not /////
+                if (start[string_idx] == 'L') { return false; }
+                else { ; }
+            }
+
+        } //Left
+
+        for (int string_idx = (len_string - 1); (string_idx >= 0); (string_idx--)) //Right
+        {
+            ///// Check if the current indexed-char matched conditions or not /////
+            if (target[string_idx] == 'R') { record_right_stack.push('R'); } //Keep updating, recording
+            else { ; }
+
+            ///// Check if the current stack is empty or not /////
+            if (! record_right_stack.empty())
+            {
+                ///// Check if the current indexed-char matched conditions or not /////
+                if ((start[string_idx] == 'L') || (target[string_idx] == 'L')) { return false; }
+                else if (start[string_idx] == 'R') { record_right_stack.pop(); } //Keep updating, popped
+                else { ; }
+            }
+
+            else
+            {
+                ///// Check if the current indexed-char matched conditions or not /////
+                if (start[string_idx] == 'R') { return false; }
+                else { ; }
+            }
+
+        } //Right
+
+        return (record_left_stack.empty() && record_right_stack.empty()) ? true : false;
+    }
+};
