72d974e9e9fd6b6cc6cd18068ecdf21f1f3ab658
simon <31026907+simonyang0608@users.noreply.github.com>
2025-01-11 21:38:08 +0800

    [Add] Add array title no.73 with python language

---
+++ Array/3158.Find_the_XOR_of_Numbers_Which_Appear_Twice/Python/3158.Find_the_XOR_of_Numbers_Which_Appear_Twice.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def duplicateNumbersXOR(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #===============================#
+        # Greedy-based traversal method #
+        #===============================#
+
+        ############
+        #Initialize
+        ##### Length of nums array #####
+        len_nums = len(nums)
+
+        ##### Record traversal pointer #####
+        record_trav_ptr = 0
+
+        ##### Result summary value #####
+        res_sum_val = 0
+
+
+        #############################
+        #Greedy-based loop traversal
+        nums.sort() #Sorted operations
+
+        while (record_trav_ptr < len_nums):
+            record_cnter = 1 #Record summary counter
+
+            while (((record_trav_ptr + 1) < len_nums) and ((nums[record_trav_ptr] == nums[(record_trav_ptr + 1)]))):
+                record_cnter += 1 #Keep updating/accumulating
+
+                record_trav_ptr += 1 #Keep updating/traversing
+
+            ##### Check if the current summary counter is less than 2 or not #####
+            if (record_cnter == 2):
+                res_sum_val ^= nums[record_trav_ptr] #Keep updating/accumulating
+            else:
+                pass
+
+            record_trav_ptr += 1 #Keep updating/traversing
+
+        return res_sum_val
