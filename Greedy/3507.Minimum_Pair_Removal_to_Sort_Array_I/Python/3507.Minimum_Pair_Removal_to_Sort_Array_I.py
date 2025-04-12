7e08168a6c33503aa9d594728b1084ba7d0fe85c
simon <31026907+simonyang0608@users.noreply.github.com>
2025-04-12 12:56:43 +0800

    [Add] Add greedy title no.29 with python language

--- Greedy/3507.Minimum_Pair_Removal_to_Sort_Array_I/Python/3507.Minimum_Pair_Removal_to_Sort_Array_I.py
+++ Greedy/3507.Minimum_Pair_Removal_to_Sort_Array_I/Python/3507.Minimum_Pair_Removal_to_Sort_Array_I.py
@@ -1,88 +1,76 @@
-class Solution {
-public:
-    int minimumPairRemoval(vector<int> & nums) 
-    {
-        //====================================//
-        // Input type:                        //
-        //   - vector<int> ref. (nums vector) //
-        // Return type:                       //
-        //   - int (result minimun steps)     //
-        //====================================//
+class Solution(object):
+    def minimumPairRemoval(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #===============================#
+        # Greedy-based traversal method #
+        #===============================#
 
-        /*Initialize*/
-        ///// Length of nums vector /////
-        int len_nums = nums.size();
+        ############
+        #Initialize
+        ##### Length of nums array #####
+        len_nums = len(nums)
 
-        ///// Result minimun steps /////
-        int res_min_steps = 0;
-
-
-        /*Greedy-based loop traversal*/
-        while (! is_sorted(nums, len_nums)) //Cycled-times
-        {
-            int record_idx = (-1); int record_pair_sum = INT_MAX; //Record index, pair-summary
-
-            bool record_flag = false; //Record flag
-
-            for (int nums_idx = 0; (nums_idx < (len_nums - 1)); (nums_idx++)) //Whole
-            {
-                ///// Check if the current pair-summary matched conditions or not /////
-                if ((nums[nums_idx] + nums[(nums_idx + 1)]) < record_pair_sum)
-                {
-                    record_pair_sum = (nums[nums_idx] + nums[(nums_idx + 1)]); //Keep updating, overwriting
-
-                    record_idx = nums_idx; //Keep updating, overwriting
-                }
-                else { ; }
-
-            } //Whole
-
-            for (int nums_idx = 0; (nums_idx < (len_nums - 1)); (nums_idx++)) //Whole
-            {
-                ///// Check if the current flag matched conditions or not /////
-                if (! record_flag)
-                {
-                    ///// Check if the current index matched conditions or not /////
-                    if (record_idx == nums_idx)
-                    {
-                        nums[nums_idx] += nums[(nums_idx + 1)]; //Keep updating, accumulating
-
-                        record_flag = true; //Keep updating, overwriting
-                    }
-                    else { ; }
-                }
-                else { nums[nums_idx] = nums[(nums_idx + 1)]; } //Keep updating, overwriting
-
-            } //Whole
-
-            (res_min_steps++); //Keep updating, accumulating
-
-            nums.pop_back(); (len_nums--); //Keep updating, reducing
-
-        } //Cycled-times
-
-        return res_min_steps;
-    }
+        ##### Result minimun steps #####
+        res_min_steps = 0
 
 
-    bool is_sorted(const vector<int> & nums, const int & len_nums)
-    {
-        //============================================//
-        // Input type:                                //
-        //   - const vector<int> ref. (nums vector)   //
-        //   - const int ref. (length of nums vector) //
-        // Return type:                               //
-        //   - bool (result flag)                     //
-        //============================================//
+        #############################
+        #Greedy-based loop traversal
+        while (not self.is_sorted(nums, len_nums)):
+            record_idx, record_pair_sum = (-1), float("inf") #Record index, pair-summary
 
-        /*Whole process, flow*/
-        for (int nums_idx = 0; (nums_idx < (len_nums - 1)); (nums_idx++)) //Whole
-        {
-            ///// Check if the current indexed-value matched conditions or not /////
-            if ((nums[nums_idx]) > nums[(nums_idx + 1)]) { return false; }
+            record_flag = False #Record flag
 
-        } //Whole
+            for nums_idx in range((len_nums - 1)):
 
-        return true;
-    }
-};
+                ##### Check if the current pair-summary matched conditions or not #####
+                if ((nums[nums_idx] + nums[(nums_idx + 1)]) < record_pair_sum):
+                    record_pair_sum = (nums[nums_idx] + nums[(nums_idx + 1)]) #Keep updating/overwriting
+
+                    record_idx = nums_idx #Keep updating/overwriting
+                else:
+                    pass
+
+            for nums_idx in range((len_nums - 1)):
+
+                ##### Check if the current flag matched conditions or not #####
+                if (not record_flag):
+
+                    ##### Check if the current index matched conditions or not #####
+                    if (nums_idx == record_idx):
+                        nums[nums_idx] += nums[(nums_idx + 1)] #Keep updating/accumulating
+
+                        record_flag |= True #Keep updating/overwriting
+                    else:
+                        pass
+                else:
+                    nums[nums_idx] = nums[(nums_idx + 1)] #Keep updating/overwriting
+
+            res_min_steps += 1 #Keep updating/accumulating
+
+            nums.pop() #Keep updating/popped
+
+            len_nums -= 1 #Keep updating/reducing
+
+        return res_min_steps
+
+
+    def is_sorted(self, nums, len_nums):
+        """
+        :type nums: List[int]
+        :type len_nums: int
+        :rtype: bool
+        """
+
+        #####################
+        #Whole process/flow
+        for nums_idx in range((len_nums - 1)):
+
+            ##### Check if the current indexed-value matched conditions or not #####
+            if (nums[nums_idx] > nums[(nums_idx + 1)]):
+                return False
+
+        return True
